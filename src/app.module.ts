1d87acde7e5e479d74c78e1550d3b5b28c538fbc
Min <minkim.dev@gmail.com>
2018-09-14 00:11:31 -0400

    Adding user module components

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import {UserModule} from './user/user.module';
 
 @Module({
   imports: [
       TypeOrmModule.forRoot(),
       AuthModule,
+      UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
