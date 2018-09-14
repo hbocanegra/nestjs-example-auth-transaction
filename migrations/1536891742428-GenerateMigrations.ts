f3209af707ce5260a91d8dfc14f835b7c75274d3
Min <minkim.dev@gmail.com>
2018-09-13 23:27:05 -0400

    Changing the id type as uuid

--- migrations/1536891742428-GenerateMigrations.ts
+++ migrations/1536891742428-GenerateMigrations.ts
@@ -1,12 +1,15 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class GenerateMigrations1536813004553 implements MigrationInterface {
+export class GenerateMigrations1536891742428 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<any> {
         await queryRunner.query("CREATE TABLE `photo` (`id` int NOT NULL AUTO_INCREMENT, `name` varchar(500) NOT NULL, `description` text NOT NULL, `filename` varchar(255) NOT NULL, `views` int NOT NULL, `isPublished` tinyint NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB");
+        await queryRunner.query("CREATE TABLE `user` (`id` varchar(255) NOT NULL, `username` varchar(50) NOT NULL, `password` varchar(100) NULL, `passwordHash` varchar(100) NULL, `email` varchar(500) NOT NULL, UNIQUE INDEX `IDX_78a916df40e02a9deb1c4b75ed` (`username`), PRIMARY KEY (`id`)) ENGINE=InnoDB");
     }
 
     public async down(queryRunner: QueryRunner): Promise<any> {
+        await queryRunner.query("DROP INDEX `IDX_78a916df40e02a9deb1c4b75ed` ON `user`");
+        await queryRunner.query("DROP TABLE `user`");
         await queryRunner.query("DROP TABLE `photo`");
     }
 
